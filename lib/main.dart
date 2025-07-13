e008b294e9ea8ce7c4e669409090c52ee42e8a40
Mike <eric.wietstruk@gmail.com>
2025-07-12 17:05:26 -0700

    Testing window sizing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:window_size/window_size.dart';
 
@@ -18,16 +22,69 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: _buildHome(title),
+      home: _buildHome(title, context),
     );
   }
 
-  Widget _buildHome(String title) {
+  Widget _buildHome(String title, context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Container(),
+      body: Row(
+        children: [
+          SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.25,
+            child: Column(
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                Expanded(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: const Row(
+                      children: [Text('This is where page navigation goes')],
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+                Expanded(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: const Row(
+                      children: [Text('This is where saved projects go')],
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              ],
+            ),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.10),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.61,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration:
+                          BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: const Row(
+                      children: [
+                        Text('This is where the current page is displayed'),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              ],
+            ),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.02),
+        ],
+      ),
     );
   }
 }
